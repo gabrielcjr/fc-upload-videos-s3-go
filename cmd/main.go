4c733ffd94cc78f4240d8ac0a59568c5af7dc2db
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2023-02-07 13:08:27 -0300

    add to main

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,9 @@
 package main
+
+import "github.com/gacarneirojr/fc-upload-videos-s3-go/internal"
+
+func main() {
+
+	internal.File("drmsonus", "1")
+
+}
---
+++ internal/cli-questions.go
@@ -0,0 +1,17 @@
+package internal
+
+type REPOS struct {
+	repos map[string]string
+}
+
+repos REPOS := {
+	"TYPESCRIPT": "code/fullcycle/fc3/microsservico-catalogo-de-videos-com-typescript/",
+	"DOTNET": "code/fullcycle/fc3/microsservico-catalogo-de-videos-com-dotnet/",
+	"REACT": "code/fullcycle/fc3/microsservico-administracao-do-catalogo-de-videos-com-React/",
+	"JAVA": "code/fullcycle/fc3/microsservico-catalogo-de-videos-com-java-new/",
+	"PHP": "code/fullcycle/fc3/microsservico-catalogo-de-videos-com-php/",
+	"PYTHON": "code/fullcycle/fc3/microsservico-catalogo-de-videos-com-python/",
+	"DEPLOY_CLOUDS": "code/fullcycle/Deploy-das-Cloud-Providers/",
+	"GOLANG": "code/go/",
+	"EDA": "code/fullcycle/fc3/EDA/"
+  }
